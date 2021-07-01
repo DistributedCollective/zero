748ba949707ee1f093794ac19073fbd0c62e3175
guidota <gtamborindeguy@atixlabs.com>
2021-07-01 12:14:49 -0300

    feat: separate LUSDToken storage from logic

--- packages/contracts/contracts/LUSDToken.sol
+++ packages/contracts/contracts/LUSDToken.sol
@@ -6,6 +6,7 @@ import "./Interfaces/ILUSDToken.sol";
 import "./Dependencies/SafeMath.sol";
 import "./Dependencies/CheckContract.sol";
 import "./Dependencies/console.sol";
+import "./LUSDTokenStorage.sol";
 /*
 *
 * Based upon OpenZeppelin's ERC20 contract:
@@ -24,40 +25,7 @@ import "./Dependencies/console.sol";
 * 2) sendToPool() and returnFromPool(): functions callable only Liquity core contracts, which move LUSD tokens between Liquity <-> user.
 */
 
-contract LUSDToken is CheckContract, ILUSDToken {
-    using SafeMath for uint256;
-    
-    uint256 private _totalSupply;
-    string constant internal _NAME = "LUSD Stablecoin";
-    string constant internal _SYMBOL = "LUSD";
-    string constant internal _VERSION = "1";
-    uint8 constant internal _DECIMALS = 18;
-    
-    // --- Data for EIP2612 ---
-    
-    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    bytes32 private constant _PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-    // keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
-    bytes32 private constant _TYPE_HASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
-
-    // Cache the domain separator as an immutable value, but also store the chain id that it corresponds to, in order to
-    // invalidate the cached domain separator if the chain id changes.
-    bytes32 private immutable _CACHED_DOMAIN_SEPARATOR;
-    uint256 private immutable _CACHED_CHAIN_ID;
-
-    bytes32 private immutable _HASHED_NAME;
-    bytes32 private immutable _HASHED_VERSION;
-    
-    mapping (address => uint256) private _nonces;
-    
-    // User data for LUSD token
-    mapping (address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;  
-    
-    // --- Addresses ---
-    address public immutable troveManagerAddress;
-    address public immutable stabilityPoolAddress;
-    address public immutable borrowerOperationsAddress;
+contract LUSDToken is LUSDTokenStorage, CheckContract, ILUSDToken {
     
     // --- Events ---
     event TroveManagerAddressChanged(address _troveManagerAddress);
