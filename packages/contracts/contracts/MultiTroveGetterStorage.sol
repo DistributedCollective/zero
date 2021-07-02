655049b357c756762282c34707db476ae956c95a
Swamp Thing <swamp.thing.sovryn@gmail.com>
2021-07-02 15:04:48 -0300

    feat: MultiTroveGetter storage separated from logic

---
+++ packages/contracts/contracts/MultiTroveGetterStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.11;
+import "./TroveManager.sol";
+import "./SortedTroves.sol";
+import "./Dependencies/Ownable.sol";
+
+contract MultiTroveGetterStorage is Ownable {
+
+    TroveManager public troveManager; // XXX Troves missing from ITroveManager?
+    ISortedTroves public sortedTroves;
+
+}
