4b6c6ca33ee0f9905a12b1d3b858a261a5caa441
guidota <gtamborindeguy@atixlabs.com>
2021-07-02 15:15:04 -0300

    feat: SortedTroves storage separated from logic

--- packages/contracts/contracts/SortedTroves.sol
+++ packages/contracts/contracts/SortedTroves.sol
@@ -6,7 +6,7 @@ import "./Interfaces/ISortedTroves.sol";
 import "./Interfaces/ITroveManager.sol";
 import "./Interfaces/IBorrowerOperations.sol";
 import "./Dependencies/SafeMath.sol";
-import "./Dependencies/Ownable.sol";
+import "./SortedTrovesStorage.sol";
 import "./Dependencies/CheckContract.sol";
 import "./Dependencies/console.sol";
 
@@ -43,37 +43,12 @@ import "./Dependencies/console.sol";
 *
 * - Public functions with parameters have been made internal to save gas, and given an external wrapper function for external access
 */
-contract SortedTroves is Ownable, CheckContract, ISortedTroves {
-    using SafeMath for uint256;
-
-    string constant public NAME = "SortedTroves";
-
+contract SortedTroves is SortedTrovesStorage, CheckContract, ISortedTroves {
+    
     event TroveManagerAddressChanged(address _troveManagerAddress);
     event BorrowerOperationsAddressChanged(address _borrowerOperationsAddress);
     event NodeAdded(address _id, uint _NICR);
     event NodeRemoved(address _id);
-
-    address public borrowerOperationsAddress;
-
-    ITroveManager public troveManager;
-
-    // Information for a node in the list
-    struct Node {
-        bool exists;
-        address nextId;                  // Id of next node (smaller NICR) in the list
-        address prevId;                  // Id of previous node (larger NICR) in the list
-    }
-
-    // Information for the list
-    struct Data {
-        address head;                        // Head of the list. Also the node in the list with the largest NICR
-        address tail;                        // Tail of the list. Also the node in the list with the smallest NICR
-        uint256 maxSize;                     // Maximum size of the list
-        uint256 size;                        // Current size of the list
-        mapping (address => Node) nodes;     // Track the corresponding ids for each node in the list
-    }
-
-    Data public data;
 
     // --- Dependency setters ---
 
