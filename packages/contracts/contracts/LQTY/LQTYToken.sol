5514c06fb7fdd13895d21c1de3cf06d86c2f1076
Swamp Thing <swamp.thing.sovryn@gmail.com>
2021-07-05 11:57:04 -0300

    feat: LQTYToken storage separated from logic

--- packages/contracts/contracts/LQTY/LQTYToken.sol
+++ packages/contracts/contracts/LQTY/LQTYToken.sol
@@ -7,6 +7,7 @@ import "../Dependencies/SafeMath.sol";
 import "../Interfaces/ILQTYToken.sol";
 import "../Interfaces/ILockupContractFactory.sol";
 import "../Dependencies/console.sol";
+import "./LQTYTokenStorage.sol";
 
 /*
 * Based upon OpenZeppelin's ERC20 contract:
@@ -47,53 +48,8 @@ import "../Dependencies/console.sol";
 * and the multisig has the same rights as any other address.
 */
 
-contract LQTYToken is CheckContract, ILQTYToken {
+contract LQTYToken is LQTYTokenStorage, CheckContract, ILQTYToken {
     using SafeMath for uint256;
-
-    // --- ERC20 Data ---
-
-    string constant internal _NAME = "LQTY";
-    string constant internal _SYMBOL = "LQTY";
-    string constant internal _VERSION = "1";
-    uint8 constant internal  _DECIMALS = 18;
-
-    mapping (address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;
-    uint private _totalSupply;
-
-    // --- EIP 2612 Data ---
-
-    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    bytes32 private constant _PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-    // keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
-    bytes32 private constant _TYPE_HASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
-
-    // Cache the domain separator as an immutable value, but also store the chain id that it corresponds to, in order to
-    // invalidate the cached domain separator if the chain id changes.
-    bytes32 private immutable _CACHED_DOMAIN_SEPARATOR;
-    uint256 private immutable _CACHED_CHAIN_ID;
-
-    bytes32 private immutable _HASHED_NAME;
-    bytes32 private immutable _HASHED_VERSION;
-    
-    mapping (address => uint256) private _nonces;
-
-    // --- LQTYToken specific data ---
-
-    uint public constant ONE_YEAR_IN_SECONDS = 31536000;  // 60 * 60 * 24 * 365
-
-    // uint for use with SafeMath
-    uint internal _1_MILLION = 1e24;    // 1e6 * 1e18 = 1e24
-
-    uint internal immutable deploymentStartTime;
-    address public immutable multisigAddress;
-
-    address public immutable communityIssuanceAddress;
-    address public immutable lqtyStakingAddress;
-
-    uint internal immutable lpRewardsEntitlement;
-
-    ILockupContractFactory public immutable lockupContractFactory;
 
     // --- Events ---
 
