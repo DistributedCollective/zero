ab7a0ecf96ae37fdf7cbaafd0312354c54588e72
Swamp Thing <swamp.thing.sovryn@gmail.com>
2021-07-05 11:50:03 -0300

    feat: LQTYStaking storage separated from logic

--- packages/contracts/contracts/LQTY/LQTYStaking.sol
+++ packages/contracts/contracts/LQTY/LQTYStaking.sol
@@ -11,33 +11,10 @@ import "../Interfaces/ILQTYToken.sol";
 import "../Interfaces/ILQTYStaking.sol";
 import "../Dependencies/LiquityMath.sol";
 import "../Interfaces/ILUSDToken.sol";
+import "./LQTYStakingStorage.sol";
 
-contract LQTYStaking is ILQTYStaking, Ownable, CheckContract, BaseMath {
+contract LQTYStaking is LQTYStakingStorage, ILQTYStaking, CheckContract, BaseMath {
     using SafeMath for uint;
-
-    // --- Data ---
-    string constant public NAME = "LQTYStaking";
-
-    mapping( address => uint) public stakes;
-    uint public totalLQTYStaked;
-
-    uint public F_ETH;  // Running sum of ETH fees per-LQTY-staked
-    uint public F_LUSD; // Running sum of LQTY fees per-LQTY-staked
-
-    // User snapshots of F_ETH and F_LUSD, taken at the point at which their latest deposit was made
-    mapping (address => Snapshot) public snapshots; 
-
-    struct Snapshot {
-        uint F_ETH_Snapshot;
-        uint F_LUSD_Snapshot;
-    }
-    
-    ILQTYToken public lqtyToken;
-    ILUSDToken public lusdToken;
-
-    address public troveManagerAddress;
-    address public borrowerOperationsAddress;
-    address public activePoolAddress;
 
     // --- Events ---
 
