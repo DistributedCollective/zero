8c07fa79d77edab6b4641e825dac7e8d8596e409
Guido Tamborindeguy <gtamborindeguy@atixlabs.com>
2021-07-07 13:55:02 +0000

    feat: CommunityIssuance storage separated from logic

--- packages/contracts/contracts/LQTY/CommunityIssuance.sol
+++ packages/contracts/contracts/LQTY/CommunityIssuance.sol
@@ -9,47 +9,11 @@ import "../Dependencies/LiquityMath.sol";
 import "../Dependencies/Ownable.sol";
 import "../Dependencies/CheckContract.sol";
 import "../Dependencies/SafeMath.sol";
+import "./CommunityIssuanceStorage.sol";
 
 
-contract CommunityIssuance is ICommunityIssuance, Ownable, CheckContract, BaseMath {
+contract CommunityIssuance is CommunityIssuanceStorage, ICommunityIssuance, CheckContract, BaseMath {
     using SafeMath for uint;
-
-    // --- Data ---
-
-    string constant public NAME = "CommunityIssuance";
-
-    uint constant public SECONDS_IN_ONE_MINUTE = 60;
-
-   /* The issuance factor F determines the curvature of the issuance curve.
-    *
-    * Minutes in one year: 60*24*365 = 525600
-    *
-    * For 50% of remaining tokens issued each year, with minutes as time units, we have:
-    * 
-    * F ** 525600 = 0.5
-    * 
-    * Re-arranging:
-    * 
-    * 525600 * ln(F) = ln(0.5)
-    * F = 0.5 ** (1/525600)
-    * F = 0.999998681227695000 
-    */
-    uint constant public ISSUANCE_FACTOR = 999998681227695000;
-
-    /* 
-    * The community LQTY supply cap is the starting balance of the Community Issuance contract.
-    * It should be minted to this contract by LQTYToken, when the token is deployed.
-    * 
-    * Set to 32M (slightly less than 1/3) of total LQTY supply.
-    */
-    uint constant public LQTYSupplyCap = 32e24; // 32 million
-
-    ILQTYToken public lqtyToken;
-
-    address public stabilityPoolAddress;
-
-    uint public totalLQTYIssued;
-    uint public immutable deploymentTime;
 
     // --- Events ---
 
@@ -58,10 +22,6 @@ contract CommunityIssuance is ICommunityIssuance, Ownable, CheckContract, BaseMa
     event TotalLQTYIssuedUpdated(uint _totalLQTYIssued);
 
     // --- Functions ---
-
-    constructor() public {
-        deploymentTime = block.timestamp;
-    }
 
     function setAddresses
     (
@@ -75,6 +35,7 @@ contract CommunityIssuance is ICommunityIssuance, Ownable, CheckContract, BaseMa
         checkContract(_lqtyTokenAddress);
         checkContract(_stabilityPoolAddress);
 
+        deploymentTime = block.timestamp;
         lqtyToken = ILQTYToken(_lqtyTokenAddress);
         stabilityPoolAddress = _stabilityPoolAddress;
 
