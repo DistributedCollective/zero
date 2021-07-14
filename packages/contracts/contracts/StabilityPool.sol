6175a3a14ab87da890e08655a724072a05021c73
Swamp Thing <swamp.thing.sovryn@gmail.com>
2021-07-14 11:40:31 -0300

    feat: StabilityPool storage separated from logic

--- packages/contracts/contracts/StabilityPool.sol
+++ packages/contracts/contracts/StabilityPool.sol
@@ -10,11 +10,10 @@ import './Interfaces/ILUSDToken.sol';
 import './Interfaces/ISortedTroves.sol';
 import "./Interfaces/ICommunityIssuance.sol";
 import "./Dependencies/LiquityBase.sol";
-import "./Dependencies/SafeMath.sol";
 import "./Dependencies/LiquitySafeMath128.sol";
-import "./Dependencies/Ownable.sol";
 import "./Dependencies/CheckContract.sol";
 import "./Dependencies/console.sol";
+import "./StabilityPoolStorage.sol";
 
 /*
  * The Stability Pool holds LUSD tokens deposited by Stability Pool depositors.
@@ -145,94 +144,8 @@ import "./Dependencies/console.sol";
  * The product P (and snapshot P_t) is re-used, as the ratio P/P_t tracks a deposit's depletion due to liquidations.
  *
  */
-contract StabilityPool is LiquityBase, Ownable, CheckContract, IStabilityPool {
+contract StabilityPool is StabilityPoolStorage, LiquityBase, CheckContract, IStabilityPool {
     using LiquitySafeMath128 for uint128;
-
-    string constant public NAME = "StabilityPool";
-
-    IBorrowerOperations public borrowerOperations;
-
-    ITroveManager public troveManager;
-
-    ILUSDToken public lusdToken;
-
-    // Needed to check if there are pending liquidations
-    ISortedTroves public sortedTroves;
-
-    ICommunityIssuance public communityIssuance;
-
-    uint256 internal ETH;  // deposited ether tracker
-
-    // Tracker for LUSD held in the pool. Changes when users deposit/withdraw, and when Trove debt is offset.
-    uint256 internal totalLUSDDeposits;
-
-   // --- Data structures ---
-
-    struct FrontEnd {
-        uint kickbackRate;
-        bool registered;
-    }
-
-    struct Deposit {
-        uint initialValue;
-        address frontEndTag;
-    }
-
-    struct Snapshots {
-        uint S;
-        uint P;
-        uint G;
-        uint128 scale;
-        uint128 epoch;
-    }
-
-    mapping (address => Deposit) public deposits;  // depositor address -> Deposit struct
-    mapping (address => Snapshots) public depositSnapshots;  // depositor address -> snapshots struct
-
-    mapping (address => FrontEnd) public frontEnds;  // front end address -> FrontEnd struct
-    mapping (address => uint) public frontEndStakes; // front end address -> last recorded total deposits, tagged with that front end
-    mapping (address => Snapshots) public frontEndSnapshots; // front end address -> snapshots struct
-
-    /*  Product 'P': Running product by which to multiply an initial deposit, in order to find the current compounded deposit,
-    * after a series of liquidations have occurred, each of which cancel some LUSD debt with the deposit.
-    *
-    * During its lifetime, a deposit's value evolves from d_t to d_t * P / P_t , where P_t
-    * is the snapshot of P taken at the instant the deposit was made. 18-digit decimal.
-    */
-    uint public P = DECIMAL_PRECISION;
-
-    uint public constant SCALE_FACTOR = 1e9;
-
-    // Each time the scale of P shifts by SCALE_FACTOR, the scale is incremented by 1
-    uint128 public currentScale;
-
-    // With each offset that fully empties the Pool, the epoch is incremented by 1
-    uint128 public currentEpoch;
-
-    /* ETH Gain sum 'S': During its lifetime, each deposit d_t earns an ETH gain of ( d_t * [S - S_t] )/P_t, where S_t
-    * is the depositor's snapshot of S taken at the time t when the deposit was made.
-    *
-    * The 'S' sums are stored in a nested mapping (epoch => scale => sum):
-    *
-    * - The inner mapping records the sum S at different scales
-    * - The outer mapping records the (scale => sum) mappings, for different epochs.
-    */
-    mapping (uint128 => mapping(uint128 => uint)) public epochToScaleToSum;
-
-    /*
-    * Similarly, the sum 'G' is used to calculate LQTY gains. During it's lifetime, each deposit d_t earns a LQTY gain of
-    *  ( d_t * [G - G_t] )/P_t, where G_t is the depositor's snapshot of G taken at time t when  the deposit was made.
-    *
-    *  LQTY reward events occur are triggered by depositor operations (new deposit, topup, withdrawal), and liquidations.
-    *  In each case, the LQTY reward is issued (i.e. G is updated), before other state changes are made.
-    */
-    mapping (uint128 => mapping(uint128 => uint)) public epochToScaleToG;
-
-    // Error tracker for the error correction in the LQTY issuance calculation
-    uint public lastLQTYError;
-    // Error trackers for the error correction in the offset calculation
-    uint public lastETHError_Offset;
-    uint public lastLUSDLossError_Offset;
 
     // --- Events ---
 
