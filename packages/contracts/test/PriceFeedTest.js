11150357bf5fdef0783d87f55de8731bd9604746
Swamp Thing <swamp.thing.sovryn@gmail.com>
2021-07-19 13:51:08 +0000

    feat: make price feed upgradable

--- packages/contracts/test/PriceFeedTest.js
+++ packages/contracts/test/PriceFeedTest.js
@@ -15,6 +15,7 @@ contract("PriceFeed", async accounts => {
   let zeroAddressPriceFeed;
   let mockedMoCPriceFeed;
   let mockedRskOracle;
+  let anotherOracle;
 
   const setAddresses = async () => {
     await priceFeed.setAddresses(mockedMoCPriceFeed.address, mockedRskOracle.address, {
@@ -36,6 +37,9 @@ contract("PriceFeed", async accounts => {
     ExternalPriceFeedTester.setAsDeployed(mockedMoCPriceFeed);
 
     mockedRskOracle = await ExternalPriceFeedTester.new();
+    ExternalPriceFeedTester.setAsDeployed(mockedRskOracle);
+
+    anotherOracle = await ExternalPriceFeedTester.new();
     ExternalPriceFeedTester.setAsDeployed(mockedRskOracle);
 
     //Set current and prev prices in both oracles
@@ -68,7 +72,7 @@ contract("PriceFeed", async accounts => {
   });
 
   describe("Mainnet PriceFeed setup", async accounts => {
-    it("fetchPrice should fail on contract with no medianizer address set", async () => {
+    it("fetchPrice should fail on contract with no main price feed address set", async () => {
       try {
         const price = await zeroAddressPriceFeed.fetchPrice();
         assert.isFalse(price.receipt.status);
@@ -77,7 +81,7 @@ contract("PriceFeed", async accounts => {
       }
     });
 
-    it("fetchPrice should fail on contract with no rsk oracle address set", async () => {
+    it("fetchPrice should fail on contract with no backup oracle address set", async () => {
       try {
         const price = await zeroAddressPriceFeed.fetchPrice();
         assert.isFalse(price.receipt.status);
@@ -89,11 +93,61 @@ contract("PriceFeed", async accounts => {
     it("setAddresses should fail whe called by nonOwner", async () => {
       await assertRevert(
         priceFeed.setAddresses(mockedMoCPriceFeed.address, mockedRskOracle.address, { from: alice }),
-        "Ownable:: access denied"
+        "Ownable: caller is not the owner"
       );
     });
 
-    it("should set the price when setting the medianizer oracle", async () => {
+    it("setAddress should fail when called by nonOwner", async () => {
+      await assertRevert(
+        priceFeed.setAddress(0, mockedMoCPriceFeed.address, { from: alice }),
+        "Ownable: caller is not the owner"
+      );
+    });
+
+    it("setAddress should fail when called on an invalid index", async () => {
+      await assertRevert(
+        priceFeed.setAddress(2, mockedMoCPriceFeed.address),
+        "Out of bounds when setting the price feed"
+      );
+    });
+
+    it("setAddress should fail whe called using an invalid contract", async () => {
+      await assertRevert(
+        priceFeed.setAddress(0, "0x0000000000000000000000000000000000000000"),
+        "Ownable: caller is not the owner"
+      );
+    });
+
+    it("should let price feeds to be changed", async () => {
+      await mockedMoCPriceFeed.setLatestAnswer(dec(10, 18), true);
+      await mockedRskOracle.setLatestAnswer(dec(12, 18), true);
+
+      const txOwner = await priceFeed.setAddresses(
+        mockedMoCPriceFeed.address,
+        mockedRskOracle.address,
+        { from: owner }
+      );
+      assert.isTrue(txOwner.receipt.status);
+
+      let price = await priceFeed.lastGoodPrice();
+      assert.equal(price, dec(10, 18));
+
+      await mockedMoCPriceFeed.setLatestAnswer(dec(15, 18), true);
+      // it still uses the main price feed
+      await priceFeed.fetchPrice();
+      price = await priceFeed.lastGoodPrice();
+      assert.equal(price, dec(15, 18));
+
+      // swap an oracle
+      await anotherOracle.setLatestAnswer(dec(14, 18), true);
+      await priceFeed.setAddress(0, anotherOracle.address);
+      await priceFeed.fetchPrice();
+      price = await priceFeed.lastGoodPrice();
+      assert.equal(price, dec(14, 18));
+    });
+    
+
+    it("should set the price when setting the main price feed oracle", async () => {
       await mockedMoCPriceFeed.setLatestAnswer(dec(10, 18), true);
       await mockedRskOracle.setLatestAnswer(dec(12, 18), true);
 
@@ -108,19 +162,19 @@ contract("PriceFeed", async accounts => {
       assert.equal(price, dec(10, 18));
     });
 
-    it("should revert if the medianizer fails", async () => {
+    it("should revert if the main price feed fails", async () => {
       await mockedMoCPriceFeed.setLatestAnswer(dec(10, 18), false);
       await mockedRskOracle.setLatestAnswer(dec(12, 18), true);
 
       await assertRevert(
         priceFeed.setAddresses(mockedMoCPriceFeed.address, mockedRskOracle.address, { from: alice }),
-        "Ownable:: access denied"
+        "Ownable: caller is not the owner"
       );
     });
   });
 
   describe("Price fetching", async () => {
-    it("Should properly return the price if the medianizer works ok", async () => {
+    it("Should properly return the price if the main price feed works ok", async () => {
       setAddresses();
       await mockedMoCPriceFeed.setLatestAnswer(dec(20, 18), true);
       await mockedRskOracle.setLatestAnswer(dec(22, 18), true);
@@ -130,7 +184,7 @@ contract("PriceFeed", async accounts => {
       assert.equal(price, dec(20, 18));
     });
 
-    it("Should return the rsk price if the medianizer doesn't work", async () => {
+    it("Should return the backup price if the main price feed doesn't work", async () => {
       setAddresses();
       await mockedMoCPriceFeed.setLatestAnswer(dec(20, 18), false);
       await mockedRskOracle.setLatestAnswer(dec(22, 18), true);
