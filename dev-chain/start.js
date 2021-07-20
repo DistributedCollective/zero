92ae79b61a94dba3f1495870eb29d9b23116fcc7
Swamp Thing <Swamp Thing>
2021-07-20 16:40:22 -0300

    chore: updated local node to accept localhost connections (metamask)

--- dev-chain/start.js
+++ dev-chain/start.js
@@ -1,15 +1,15 @@
-const { spawnSync, execSync } = require("child_process");
+const { spawnSync } = require("child_process");
 const { join } = require("path");
 
-const genesisPath = join(__dirname, "rsk-dev.json");
 const nodeConfPath = join(__dirname, "node.conf");
 
 spawnSync("docker", [
   "run",
   "-d",
   "--rm",
+  ...["-v", `${nodeConfPath}:/etc/rsk/node.conf`],
   ...["--name", "regtest-node-rsk"],
   ...["-p", "4444:4444/tcp"],
   ...["-p", "30305:30305/tcp"],
-  "regtest"
+  "docker.atixlabs.com/rsk/regtest:fdaf299a72b429d3066860fbc53bbb6e89e0490b"
 ]);
