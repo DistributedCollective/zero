373f765b41ce9c451494a65caf9fc48397cdc3cb
simeon <simeon@kayabrands.com>
2022-03-21 11:43:07 +0200

    restyle card Header, Subheader and content

--- packages/dev-frontend/src/components/Stability/NoDeposit.tsx
+++ packages/dev-frontend/src/components/Stability/NoDeposit.tsx
@@ -1,5 +1,10 @@
 import React, { useCallback } from "react";
+<<<<<<< HEAD
 import { Heading, Flex, Button, Text } from "theme-ui";
+=======
+import { Card, Heading, Box, Flex, Button, Text } from "theme-ui";
+import { InfoMessage } from "../InfoMessage";
+>>>>>>> ee41cab4... restyle card Header, Subheader and content
 import { useStabilityView } from "./context/StabilityViewContext";
 import { Yield } from "./Yield";
 import { Card } from "../Card";
@@ -12,6 +17,7 @@ export const NoDeposit: React.FC = props => {
   }, [dispatchEvent]);
 
   return (
+<<<<<<< HEAD
     <Card
       heading={
         <>
@@ -29,6 +35,20 @@ export const NoDeposit: React.FC = props => {
         </Flex>
         <Button onClick={handleOpenTrove}>Deposit</Button>
       </Flex>
+=======
+    <Card>
+      <Heading>Stability Pool</Heading>
+      <Heading as="h3" sx={{fontWeight:"light"}}> You can earn RBTC by depositing ZUSD.</Heading>
+      <Box sx={{ p: [2, 3] }}>
+        <Flex variant="layout.actions">
+          <Text>You have no ZUSD in the Stability Pool. </Text>
+          <Flex sx={{ justifyContent: "flex-start", flex: 1, alignItems: "center" }}>
+            <Yield />
+          </Flex>
+          <Button onClick={handleOpenTrove}>Deposit</Button>
+        </Flex>
+      </Box>
+>>>>>>> ee41cab4... restyle card Header, Subheader and content
     </Card>
   );
 };
--- packages/dev-frontend/src/components/Trove/NoTrove.tsx
+++ packages/dev-frontend/src/components/Trove/NoTrove.tsx
@@ -1,5 +1,10 @@
 import React, { useCallback } from "react";
+<<<<<<< HEAD
 import { Heading, Flex, Button, Text } from "theme-ui";
+=======
+import { Card, Heading, Box, Flex, Button, Text } from "theme-ui";
+import { InfoMessage } from "../InfoMessage";
+>>>>>>> ee41cab4... restyle card Header, Subheader and content
 import { useTroveView } from "./context/TroveViewContext";
 import { Card } from "../Card";
 
@@ -11,6 +16,7 @@ export const NoTrove: React.FC = props => {
   }, [dispatchEvent]);
 
   return (
+<<<<<<< HEAD
     <Card
       heading={
         <>
@@ -25,6 +31,18 @@ export const NoTrove: React.FC = props => {
         <Text sx={{ fontStyle: "italic" }}>You haven't borrowed any ZUSD yet. </Text>
         <Button onClick={handleOpenTrove}>Open Line of Credit</Button>
       </Flex>
+=======
+    <Card>
+      <Heading>Line of Credit</Heading>
+      <Heading as="h3" sx={{fontWeight:"light"}}>You can borrow ZUSD by opening a Line of Credit.</Heading>
+      <Box sx={{ p: [2, 3]}}>
+
+        <Flex variant="layout.actions">
+          <Text >You haven't borrowed any ZUSD yet. </Text>
+          <Button onClick={handleOpenTrove}>Open Line of Credit</Button>
+        </Flex>
+      </Box>
+>>>>>>> ee41cab4... restyle card Header, Subheader and content
     </Card>
   );
 };
