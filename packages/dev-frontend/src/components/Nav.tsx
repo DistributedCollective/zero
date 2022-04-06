4ab12160cd8d474d22287f60ec4346bf8cce061b
simeon <simeon@kayabrands.com>
2022-04-06 08:42:57 +0300

    fix font size

--- packages/dev-frontend/src/components/Nav.tsx
+++ packages/dev-frontend/src/components/Nav.tsx
@@ -4,26 +4,25 @@ import { Link } from "./Link";
 export const Nav: React.FC = () => {
   return (
     <Box as="nav" sx={{ display: ["none", "flex"] }}>
-      <Flex sx={{flex:1}}/>
-      <Flex sx={{ justifyContent: "flex-start", alignItems:"center",my:20, flex: 2 }}>
-        <Image sx={{ height: 40, pr:20}} src="/images/zerologo.svg" alt="Zero" />
+      <Flex sx={{ flex: 1 }} />
+      <Flex sx={{ justifyContent: "flex-start", alignItems: "center", my: 20, flex: 2 }}>
+        <Image sx={{ height: 40, pr: 20 }} src="/images/zerologo.svg" alt="Zero" />
         <Link to="/">Dashboard</Link>
-        <Link sx={{ fontSize: 1 }} to="/liquidation">
-          Liquidation
-        </Link>
-        <Link sx={{ fontSize: 1 }} to="/redemption">
-          Redemption
-        </Link>
+        <Link to="/liquidation">Liquidation</Link>
+        <Link to="/redemption">Redemption</Link>
       </Flex>
-      <Flex sx={{ justifyContent: "flex-end",alignItems:"center", flex: 2 }}>
-      <NavLink href="https://wiki.sovryn.app/en/sovryn-dapp/subprotocols/zero-zusd" target="_blank">
+      <Flex sx={{ justifyContent: "flex-end", alignItems: "center", flex: 2 }}>
+        <NavLink
+          href="https://wiki.sovryn.app/en/sovryn-dapp/subprotocols/zero-zusd"
+          target="_blank"
+        >
           DOCS
         </NavLink>
         <NavLink href="https://live.sovryn.app/swap" target="_blank">
           Trade
         </NavLink>
       </Flex>
-      <Flex sx={{flex:1}}/>
+      <Flex sx={{ flex: 1 }} />
     </Box>
   );
 };
