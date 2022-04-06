840279b4f3338ccfc8ba7c0f898ef63c4b41cb2b
Peiman <peymannaderi3@yahoo.com>
2022-04-06 13:47:26 +0430

    fix: Navbar issues resolved

--- packages/dev-frontend/src/components/Nav.tsx
+++ packages/dev-frontend/src/components/Nav.tsx
@@ -1,17 +1,25 @@
-import { Flex, Box, NavLink, Image } from "theme-ui";
+import { Flex, NavLink, Image, Container } from "theme-ui";
 import { Link } from "./Link";
 
 export const Nav: React.FC = () => {
   return (
-    <Box as="nav" sx={{ display: ["none", "flex"] }}>
-      <Flex sx={{ flex: 1 }} />
-      <Flex sx={{ justifyContent: "flex-start", alignItems: "center", my: 20, flex: 2 }}>
+    <Container
+      variant="main"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        mt: 3,
+        flexDirection: ["column", "column", "row"]
+      }}
+    >
+      <Flex sx={{ alignItems: "center", flexDirection: ["column", "row"] }}>
         <Image sx={{ height: 40, pr: 20 }} src="/images/zerologo.svg" alt="Zero" />
         <Link to="/">Dashboard</Link>
         <Link to="/liquidation">Liquidation</Link>
         <Link to="/redemption">Redemption</Link>
       </Flex>
-      <Flex sx={{ justifyContent: "flex-end", alignItems: "center", flex: 2 }}>
+      <Flex sx={{ alignItems: "center", flexDirection: ["column", "row"] }}>
         <NavLink
           href="https://wiki.sovryn.app/en/sovryn-dapp/subprotocols/zero-zusd"
           target="_blank"
@@ -22,7 +30,6 @@ export const Nav: React.FC = () => {
           Trade
         </NavLink>
       </Flex>
-      <Flex sx={{ flex: 1 }} />
-    </Box>
+    </Container>
   );
 };
