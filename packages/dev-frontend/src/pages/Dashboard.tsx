52b70429e2e7709435c360f3e47a17ffff2f2a3c
Peiman <peymannaderi3@yahoo.com>
2022-05-05 14:07:37 +0430

    feat: convert zusd/xusd section added

--- packages/dev-frontend/src/pages/Dashboard.tsx
+++ packages/dev-frontend/src/pages/Dashboard.tsx
@@ -4,10 +4,12 @@ import { Trove } from "../components/Trove/Trove";
 import { Stability } from "../components/Stability/Stability";
 import { SystemStats } from "../components/SystemStats";
 import { PriceManager } from "../components/PriceManager";
+import { Convert } from "../components/Aggregator/Convert";
 
 export const Dashboard: React.FC = () => (
   <Container variant="columns">
     <Container variant="left">
+      <Convert />
       <Trove />
       <Stability />
     </Container>
