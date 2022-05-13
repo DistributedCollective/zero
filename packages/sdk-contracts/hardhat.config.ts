0f5b85cbbeac325855b8bc479e2863dd45107813
Tyrone Johnson <tjcloa@gmail.com>
2022-05-13 13:02:31 +0300

    fix hh config external-artifacts

--- packages/sdk-contracts/hardhat.config.ts
+++ packages/sdk-contracts/hardhat.config.ts
@@ -79,18 +79,16 @@ export default {
     freshOutput: true,
   },
   external: {
-    external: {
-      contracts: [
-        {
-          artifacts: "external-artifacts/*.json",
-        },
-      ],
-    },
+    contracts: [
+      {
+        artifacts: "external-artifacts",
+      },
+    ],
   },
   typechain: {
     outDir: "types",
     target: "ethers-v5",
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
-    externalArtifacts: ["external-artifacts/*.json"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    externalArtifacts: ["external-artifacts/*.sol/!(*.dbg.json)"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
   },
 } as HardhatUserConfig;
