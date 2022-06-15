5112113df65363042c6b4010cf1007b8ffc96cfb
Peiman <peymannaderi3@yahoo.com>
2022-06-15 15:58:21 +0430

    trigger redeploy.

--- packages/dev-frontend/src/contracts/config.ts
+++ packages/dev-frontend/src/contracts/config.ts
@@ -11,5 +11,4 @@ const rskTestnet = {
   zusd: "0x6b41566353d6C7B8C2a7931d498F11489DacAc29",
   babelfish: "0x1572D7E4a78A8AD14AE722E6fE5f5600a2c7A149"
 };
-
 export const addresses = isMainnet ? rskMainnet : rskTestnet;
