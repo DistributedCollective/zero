5f8563642a3cfaf87c3df95e7e3e2dcfa8f4eac3
Santo Sinar Pandean <santo.pandean@gmail.com>
2022-11-09 07:21:02 +0700

    fix: remove superfloud BN conversion

--- packages/contracts/test/ProxyBorrowerWrappersScript.js
+++ packages/contracts/test/ProxyBorrowerWrappersScript.js
@@ -514,7 +514,7 @@ contract('BorrowerWrappers', async accounts => {
     const redeemedAmount = toBN(dec(100, 18))
     const sovFeeCollectorBalanceBefore = await wrbtcToken.balanceOf(sovFeeCollector)
     await th.redeemCollateral(whale, contracts, redeemedAmount)
-    const sovFeeCollectorBalanceAfter = toBN(web3.utils.toBN(await web3.eth.getBalance(sovFeeCollector)));
+    const sovFeeCollectorBalanceAfter = web3.utils.toBN(await web3.eth.getBalance(sovFeeCollector));
  
     // Alice ETH gain is ((150/2000) * (redemption fee over redeemedAmount) / price)
     const redemptionFee = await troveManager.getRedemptionFeeWithDecay(redeemedAmount)
@@ -707,7 +707,7 @@ contract('BorrowerWrappers', async accounts => {
     const redeemedAmount = toBN(dec(100, 18))
     const sovFeeCollectorBalanceBefore = await wrbtcToken.balanceOf(sovFeeCollector)
     await th.redeemCollateral(whale, contracts, redeemedAmount)
-    const sovFeeCollectorBalanceAfter = toBN(web3.utils.toBN(await web3.eth.getBalance(sovFeeCollector)));
+    const sovFeeCollectorBalanceAfter = web3.utils.toBN(await web3.eth.getBalance(sovFeeCollector));
 
     // Alice ETH gain is ((150/2000) * (redemption fee over redeemedAmount) / price)
     const redemptionFee = await troveManager.getRedemptionFeeWithDecay(redeemedAmount)
