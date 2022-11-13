763e85b2d603ea34b46c0ab5c28714c0ddb15f2f
Santo Sinar Pandean <santo.pandean@gmail.com>
2022-11-13 21:39:30 +0700

    fix: overloading function in mock feeSharingProxy

--- packages/contracts/contracts/TestContracts/MockFeeSharingProxy.sol
+++ packages/contracts/contracts/TestContracts/MockFeeSharingProxy.sol
@@ -11,13 +11,10 @@ interface MockIFeeSharingProxy {
 /// @dev Simple contract that will receive ZERO tokens issued to the SOV stakers.
 ///      see: https://github.com/DistributedCollective/Sovryn-smart-contracts/blob/b5bd57f9003ab95ab36e20859b662f6cd7a195b5/contracts/governance/FeeSharingProxy
 contract MockFeeSharingProxy is MockIFeeSharingProxy {
-
-	fallback() external payable {}
-
 	function transferTokens(address _token, uint96 _amount) override external {
 		/// Just a fake function to receive the tokens
 		ZEROToken(_token).transferFrom(msg.sender, address(this), _amount);
 	}
 
-	function transferRBTC(address _token, uint96 _amount) external payable {}
+	function transferRBTC() external payable {}
 }
