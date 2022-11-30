c54df59a7b13d6724eb567941881eb6fd239a428
Tyrone Johnson <tjcloa@gmail.com>
2022-11-30 15:50:07 +0300

    MassetTester - revert DLLR -> Nue naming

--- packages/contracts/contracts/TestContracts/MassetTester.sol
+++ packages/contracts/contracts/TestContracts/MassetTester.sol
@@ -2,64 +2,39 @@
 
 pragma solidity 0.6.11;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../BorrowerOperationsStorage.sol";
 
-contract DLLRMockToken is ERC20("Sovryn Dollar", "DLLR"), Ownable {
-    constructor () public {}
+//TODO: rename NueToken to contract DLLRMockToken is ERC20("Sovryn Dollar", "DLLR")
+contract NueToken is ERC20("Nuestro", "NUE"), Ownable {
+    constructor() public {}
 
-    function mint (address _account, uint256 _amount) onlyOwner public {
+    function mint(address _account, uint256 _amount) public onlyOwner {
         _mint(_account, _amount);
     }
 
-    function burn(address _account, uint256 _amount) onlyOwner public {
+    function burn(address _account, uint256 _amount) public onlyOwner {
         _burn(_account, _amount);
     }
 }
 
-contract MassetTester is IMasset { 
-    //TODO: add missing implementations
-    //TODO: replace existing usages of `onTokensMinted` and `redeemByBridge` and remove them
-    DLLRMockToken public token;
+contract MassetTester is IMasset {
+    NueToken public token;
 
-    constructor () public {
-        token = new DLLRMockToken();
-    }
-
-    /*function onTokensMinted(
-        uint256 _orderAmount,
-        address _tokenAddress,
-        bytes calldata _userData
-    ) external override {
-        token.mint(abi.decode(_userData, (address)), _orderAmount);
-    }*/
-
-    function redeemByBridge(
-        address _bAsset,
-        uint256 _massetQuantity,
-        address _recipient
-    ) external override returns (uint256 massetRedeemed) {
-        ERC20(_bAsset).transfer(_recipient, _massetQuantity);
-        token.burn(_recipient, _massetQuantity);
-    
-        return _massetQuantity;
+    constructor() public {
+        token = new NueToken();
     }
 
     function mintTo(
         address _bAsset,
         uint256 _bAssetQuantity,
         address _recipient
-    )
-    external override {
+    ) external override {
         IERC20(_bAsset).transferFrom(msg.sender, address(this), _bAssetQuantity);
         token.mint(_recipient, _bAssetQuantity);
     }
 
-    /*function isValidBasset(address _bAsset) external override returns(bool){
-        return true;
-    }*/
-
-    function getToken() external override view returns (address) {
+    function getToken() external view override returns (address) {
         return address(token);
     }
 
@@ -67,10 +42,10 @@ contract MassetTester is IMasset {
         address _bAsset,
         uint256 _massetQuantity,
         address _recipient
-    ) external override returns (uint256 massetRedeemed){
+    ) external override returns (uint256 massetRedeemed) {
         ERC20(_bAsset).transfer(_recipient, _massetQuantity);
         token.burn(_recipient, _massetQuantity);
-    
+
         return _massetQuantity;
     }
 }
